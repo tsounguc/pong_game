8b703190c493fb2b5a6600740a33ebf045acb9a0
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-17 19:43:59 -0400

    Create the ball class and made ball move. Add bouncing logic

---
+++ ball.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
+
+    def bounce(self):
+        self.y_move *= -1
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 
+from ball import Ball
 from paddle import Paddle
+import time
 
 screen = Screen()
 
@@ -13,6 +15,8 @@ screen.tracer(0)
 
 user_paddle = Paddle(x_position=350, y_position=0)
 computer_paddle = Paddle(x_position=-350, y_position=0)
+ball = Ball()
+
 screen.listen()
 screen.onkey(user_paddle.go_up, "Up")
 screen.onkey(user_paddle.go_down, "Down")
@@ -24,6 +28,12 @@ game_is_on = True
 while game_is_on:
     # Update animation since screen.tracer is 0
     screen.update()
+    time.sleep(0.10)
+    ball.move()
+
+    # Detect when ball hits top or bottom
+    if ball.ycor() < -290 or ball.ycor() > 290:
+        ball.bounce()
 
 screen.exitonclick()
 
